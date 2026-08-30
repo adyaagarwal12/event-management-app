7193d822418d93140bbf4dc187215fc46e84b4e7
Adya Agarwal <adyaagarwal12@gmail.com>
2026-08-30 13:07:28 +0530

    fixed server.js

--- server.js
+++ server.js
@@ -7,10 +7,19 @@ dotenv.config();
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.static("public"));
-const PORT = process.env.PORT || 5000;
 
+// Routes
+app.use("/api/events", eventRoutes);
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Campus Event Management API is running!");
+});
+
+// MongoDB connection
 mongoose
   .connect(process.env.MONGO_URI, {
     serverSelectionTimeoutMS: 5000
@@ -22,12 +31,9 @@ mongoose
     console.log("MongoDB connection error:", error.message);
   });
 
-app.get("/", (req, res) => {
-  res.send("Campus Event Management API is running!");
-});
+// Port for Render
+const PORT = process.env.PORT || 5000;
 
-app.use("/api/events", eventRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
