9785ae6b559d42f76072930b587c7276a93696ad
Adya Agarwal <adyaagarwal12@gmail.com>
2026-08-30 12:34:52 +0530

    deploying on cloud

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static("public"));
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI, {
